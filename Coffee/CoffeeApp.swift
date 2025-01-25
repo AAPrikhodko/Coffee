d6e56a8e1f46e4eb50e53bf99a57413ce4671e71
Andrei Prikhodko <prikhodko@nodasoft.com>
2025-01-25 13:58:41 +0300

    Initial Commit

---
+++ Coffee/CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp.swift
+//  Coffee
+//
+//  Created by Andrei on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
